b58b3e6a5398d7864c05598c760e60a23b158f01
Mo Deerow <mo@Mos-MacBook-Pro.local>
2016-02-29 18:25:12 +0000

    show

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,10 +3,14 @@ class RestaurantsController < ApplicationController
     @restaurants = Restaurant.all
   end
   def new
+    @restaurant = Restaurant.new
   end
   def create
     Restaurant.create(restaurant_params)
     redirect_to '/restaurants'
+  end
+  def show
+    @restaurant = Restaurant.find(params[:id])
   end
 
   def restaurant_params
