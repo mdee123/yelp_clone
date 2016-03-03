862d4485a11f13f92b32c24fdaba2599fb2bb4f7
esbaddeley <esbaddeley@gmail.com>
2016-03-03 13:00:28 +0000

    Added the method for creating with user

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,11 +7,22 @@ class ReviewsController < ApplicationController
 
   def create
     @restaurant = Restaurant.find(params[:restaurant_id])
-    @restaurant.reviews.create(review_params)
-    redirect_to restaurants_path
+    @review = @restaurant.reviews.build_with_user review_params, current_user
+
+    if @review.save
+    # @restaurant.reviews.create(review_params)
+      redirect_to restaurants_path
+    else
+      if @review.errors[:user]
+        redirect_to restaurants_path, alert: 'You have already reviewed this restaurant'
+      else
+        render :new
+      end
+    end
   end
 
   def review_params
     params.require(:review).permit(:thoughts, :rating)
   end
+
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,9 +1,12 @@
 class Restaurant < ActiveRecord::Base
 
-  has_many :reviews, dependent: :destroy
+  has_many :reviews,
+          -> { extending WithUserAssociationExtension },
+          dependent: :destroy
 
   validates :name, length: {minimum: 3}, uniqueness: true
 
   belongs_to :user
+
 
 end
